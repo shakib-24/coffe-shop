9ee1c5c17bae912cccc3e8eaf6e6843f10ca7a97
shakib-24 <mdshakib9081@gmail.com>
2025-02-27 09:52:49 +0900

    2nd commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const  navLinks = document.querySelectorAll("#.nav-menu .nav-link");
+
 const  menuOpenButton = document.querySelector("#menu-open-button");
 const  menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks=document.querySelectorAll(".nav-link")
 
 menuOpenButton.addEventListener("click",() => {
         //  Toggle mobile menu visibility 
@@ -9,22 +10,19 @@ menuOpenButton.addEventListener("click",() => {
 // Close menu when the close button is clicked 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click ());  
 
-navLinks.forEach(link => {
-        link.addEventListener("click", ()  => menuOpenButton.click() );
-})
   
 // Initialize Swiper 
 
 const swiper = new Swiper('.silder-wrapper', {
         loop: true,
-        grabCursor:true,
         spaceBetween: 25,
+        
       
         // If we need pagination
         pagination: {
           el: '.swiper-pagination',
-          clickable: true,
-          dynamicBullets: true,
+        clickable: true,
+        dynamicBullets: true,
         },
       
         // Navigation arrows
@@ -33,16 +31,17 @@ const swiper = new Swiper('.silder-wrapper', {
           prevEl: '.swiper-button-prev',
         },
 
+        // Responsive breakpoints
+        breakpoint:{
+                0:{
+                        slidesPerView: 1
+                },
+                768: {
+                        slidesPerView: 2
+                },
+                1024: {
+                        slidesPerView: 3
+                },
 
-breakpoints: {
-        0:{
-        slidesPerView:1
-},
-768:{
-        slidesPerView:2
-},
-1024:{
-        slidesPerView:3
-}
-},
+        }
       });
